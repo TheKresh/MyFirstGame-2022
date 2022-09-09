19c38722adcd663a72a6798dd73fbd352b908a93
TheKresh <viktorraboczki@gmail.com>
2022-09-09 11:42:01 +0200

    Update5_PlatformMovingBetweenPoints

---
+++ Assets/Game/Scripts/PlatformMover.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour
+{
+    [SerializeField] Transform startPos;
+    [SerializeField] Transform platformPos1;
+    [SerializeField] Transform platformPos2;
+    [SerializeField] float speed;
+
+    Vector3 moveTo;
+
+    void Start()
+    {
+        transform.position = startPos.position;
+    }
+
+    void Update()
+    {
+        if (transform.position == platformPos1.position)
+            moveTo = platformPos2.position;
+        if (transform.position == platformPos2.position)
+            moveTo = platformPos1.position;
+
+        transform.position = Vector3.MoveTowards(transform.position, moveTo, speed * Time.deltaTime);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(platformPos1.position, platformPos2.position);
+    }
+}
