ebae3ea9e12b7cc5faee85f79efc8a853b7ecec4
TheKresh <viktorraboczki@gmail.com>
2022-09-16 00:25:23 +0200

    Update7_MainMenu_GameOverMenuUpdate

--- Assets/Game/Scripts/GameOverMenu.cs
+++ Assets/Game/Scripts/GameOverMenu.cs
@@ -9,4 +9,9 @@ public class GameOverMenu : MonoBehaviour
     {
         SceneManager.LoadScene("MyFirstGame");
     }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 }
